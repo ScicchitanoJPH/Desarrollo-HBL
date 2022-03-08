43a36ac47a302ec4afb0d3c944ab014849e5634f
Diego Scicchitano <diego-sci98@hotmail.com>
2022-03-08 16:26:31 -0300

    Nueva funcionalidad que permite que luego de una actualizacion de firmware, se mantengan los parametros del JSON original,ya que antes se pisaba el JSON original con el que estaba en GitHub.

---
+++ hbl/CargarParametrosJSON.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+file_path_JSON_aux = '/home/pi/Desktop/hbl.json'
+file_path_JSON_new = '/usr/programas/hbl/modulos/hbl.json'
+
+file_path_JSON_final = '/usr/programas/hbl/modulos/hbl.json'
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) 
+with open(os.path.join(__location__ , file_path_JSON_aux), "r") as f:
+    data = json.load(f)
+    f.close()
+    print("data : ",data)
+
+print("\n\n\n\n\n")
+
+with open(os.path.join(__location__ , file_path_JSON_new), "r") as f2:
+    data2 = json.load(f2)
+    f2.close()
+    print("data2 : ",data2)
+
+for key in data:
+    print(key)
+    if key in data2:
+        print(key," pertenece a data2")
+        try:
+            for key_key in data[key]:
+                print("-----",key_key)
+                data2[key][key_key]=data[key][key_key]
+        except:
+            try:
+                data2[key]=data[key]
+            except:
+                a=0
+            a=0 
+    else:
+        print(key," no pertenece a data2") ### Si no pertenece, tiene que aregarlo?
+
+
+
+
+
+myFile = open(file_path_JSON_final, 'w')
+with myFile:
+    myFile.write(json.JSONEncoder(indent=2).encode(data2))
--- hbl/start.sh
+++ hbl/start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-sleep 4
 cd /
+sudo cp /usr/programas/hbl/modulos/hbl.json /home/pi/Desktop  ##Hago un backup del hbl.json
 cd /usr/programas
-sudo git pull origin master
+sudo git pull origin master ## Actualizo el repositorio si es que hay alguna actualizacion pendiente
+sudo python3 CargarParametrosJSON.py & ##Cargo los parametros del JSON original en el nuevo JSON
 cd /usr/programas/hbl
 sudo pigpiod &
 sudo python3 main.py &
