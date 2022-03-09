5e74c38e99d9baeba9a30a2b265f7b91c62a6636
Diego Scicchitano <diego-sci98@hotmail.com>
2022-03-09 14:43:45 -0300

    Test BackupJSON 5

--- hbl/start.sh
+++ hbl/start.sh
@@ -6,7 +6,7 @@ cd /usr/programas
 sudo git pull origin master ## Actualizo el repositorio si es que hay alguna actualizacion pendiente
 cd /
 cd /usr/programas   #Hay que volver a ingresar al path porque cuando se actualiza el repo, el path /usr/programas ya no existe mas
-sudo python3 CargarParametrosJSON.py & ##Cargo los parametros del JSON original en el nuevo JSON
+sudo python3 /usr/programas/hbl/CargarParametrosJSON.py & ##Cargo los parametros del JSON original en el nuevo JSON
 cd /usr/programas/hbl
 sudo pigpiod &
 sudo python3 main.py &
