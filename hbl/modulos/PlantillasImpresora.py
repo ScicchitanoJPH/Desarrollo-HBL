a07631e1397c76661845e8f0f8ef5cdbc0fb5c9f
Diego Scicchitano <diego-sci98@hotmail.com>
2022-08-01 13:17:09 -0300

    Agregado de excepcion a la hora de imprimir para evitar el error cuando no esta conectada la impresora

--- hbl/modulos/PlantillasImpresora.py
+++ hbl/modulos/PlantillasImpresora.py
@@ -2,10 +2,16 @@ from escpos.printer import Usb
 import time
         
 def ImpresionTest():
-    p = Usb(0x067b, 0x2305, 0, profile="TM-T88II")
-    p.text(time.strftime("%d/%m/%y") + "  -  " + time.strftime("%H:%M:%S"))
-    p.text("Hello World\n")
-    p.image("/home/pi/Desktop/JPHLogo.gif")
-    p.barcode('1324354657687', 'EAN13', 64, 2, '', '')
-    p.qr("https://www.jphlions.com/",size=8)
-    p.cut()
+    try:
+        p = Usb(0x067b, 0x2305, 0, profile="TM-T88II")
+    except Exception as e:
+        print("No se encuentra la impresora. Verifique si esta conectada.")
+    try:
+        p.text(time.strftime("%d/%m/%y") + "  -  " + time.strftime("%H:%M:%S") + "\n")
+        p.text("Hello World\n")
+        p.barcode('1324354657687', 'EAN13', 64, 2, '', '')
+        p.qr("https://www.jphlions.com/",size=8)
+        p.image("/home/pi/Desktop/JPHLogo.gif")
+        p.cut()
+    except Exception as e:
+        print("Error de Impresion")
